2f7058453ecc655affc1a645600a647161c25462
savlynn31 <savlynn31@gmail.com>
2017-04-05 19:26:19 -0400

    add contact form

--- js/scripts.js
+++ js/scripts.js
@@ -21,10 +21,10 @@
     evt.preventDefault();
     $searchField.prop("disabled",true);
     $submitButton.attr("disabled", true).val("searching....");
-    var animal = $searchField.val();
+    var website = $searchField.val();
     $('#photos').html('');
     $.getJSON(flickerAPI, {
-        tags: animal,
+        tags: website,
         format: "json"
       },
     function(data){
@@ -34,13 +34,55 @@
           photoHTML += '<li class="grid-25 tablet-grid-50">';
           photoHTML += '<a href="' + photo.link + '" class="image">';
           photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-        }); // end each
+        }); /* end each*/
       } else {
         photoHTML = "<p>No photos found that match: " + animal + ".</p>"
       }
       $('#photos').html(photoHTML);
       $searchField.prop("disabled", false);
       $submitButton.attr("disabled", false).val("Search");
-    }); // end getJSON
+    }); /* end getJSON*/
 
-  }); // end click
+  }); /* end click*/
+
+
+  /**********************
+    Submit Contact Form
+  *********************/
+
+var $form = $('#ajax-contact');
+var formMessages = $('#form-messages');
+$message.hide();
+  $form.submit(function (e) {
+    var $submitButton = $('#submitButton');
+    e.preventDefault();
+    var $formdata = $form.serialize();
+    $.ajax({
+      type: 'POST',
+      url: $form.attr('action')/*php mailer*/,
+      data: $formdata,
+    }.done(function(response) {
+      $(formMessages).removeClass('error');
+      $(formMessages).addClass('success');
+      $(formMessages).text(response);
+
+     $form.hide(); /*hides the form*/
+     $(formMessages).show(); /*showes the message*/
+
+    }).fail(function(response) {
+       $(formMessages).removeClass('success');
+       $(formMessages).addClass('error');
+        if (data.responseText !== '') {
+          $(formMessages).text(data.responseText); /*if AJAX gave an error, show it*/
+        } else {
+          $(formMessages).text('Oops! An error occured and your message could not be sent.'); 
+        }
+        $form.hide(); /*hides the form*/
+        $(formMessages).show(); /*showes the message*/
+    }); /*end AJAX*/
+
+  }); /*End Form Submit*/
+
+
+
+
